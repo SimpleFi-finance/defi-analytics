9c80ef61f2e6191143d0610308073c4b587acbe6
Goran Vladika <goran.vladika@gmail.com>
2022-06-15 16:36:18 +0200

    Implement function which prices token if it has a WETH pair in Sushi

--- sushiswap/price_helper.py
+++ sushiswap/price_helper.py
@@ -15,7 +15,7 @@ class PriceProvider:
         Sushiswap subgraph is used to fetch WETH-USDC pool reserves.
         """
 
-        query = self._load_query('usdc_eth_reserves.graphql')
+        query = self._load_query('pair_reserves.graphql')
         vars = {"block": block, "market": USDC_ETH_PAIR}
         response = self._client.execute(query, variable_values=vars)
 
@@ -28,20 +28,59 @@ class PriceProvider:
         tokenB_reserve = int(reserves[1].split("|")[2])
 
         if(tokenA == WETH):
-            return (tokenB_reserve * pow(10, -18)) / (tokenA_reserve * pow(10, -6))
+            return (tokenB_reserve * pow(10, -6)) / (tokenA_reserve * pow(10, -18))
         else:
             return (tokenA_reserve * pow(10, -6)) / (tokenB_reserve * pow(10, -18))
 
 
     def getTokenPriceinUSD(self, token, block):
         """Calculate custom token price in USD at a given block.
-        Sushiswap subgraph is used to fetch WETH-USDC pool reserves.
+        Sushiswap subgraph is used to fetch token reserves.
         """
 
-        if token == WETH:
-            return self.getEthPriceinUSD(block)
+        eth_price = self.getEthPriceinUSD(block)
+        print("eth_price = ", eth_price)
 
-        #TODO
+        if token == WETH:
+            return eth_price
+
+        weth_pair = self.getWethPairForToken(token)
+
+        print("weth_pair = ", weth_pair)
+        print("market = ", weth_pair['id'])
+
+
+        if weth_pair is None:
+            #TODO use some other pair
+            return 0
+
+        query = self._load_query('pair_reserves.graphql')
+        vars = {"block": block, "market": weth_pair['id']}
+        response = self._client.execute(query, variable_values=vars)
+
+        print("response = ", response)
+
+
+        if response['market'] is None:
+            return 0
+
+        reserves = response['market']['inputTokenTotalBalances']
+        tokenA = reserves[0].split("|")[0]
+        tokenA_reserve = int(reserves[0].split("|")[2])
+        tokenB_reserve = int(reserves[1].split("|")[2])
+
+        print("tokenA = ", tokenA)
+        print("decimalsA = ", weth_pair['inputTokens'][1]['decimals'])
+        print("decimalsB = ", weth_pair['inputTokens'][0]['decimals'])
+
+        if(tokenA == WETH):
+            token_price_in_eth = (tokenA_reserve * pow(10, -18)) /(tokenB_reserve * (-1) * int(weth_pair['inputTokens'][0]['decimals']))
+            token_price_in_usd = token_price_in_eth * eth_price
+            return token_price_in_usd
+        else:
+            token_price_in_eth = (tokenB_reserve * pow(10, -18)) / (tokenA_reserve * pow(10, (-1) * int(weth_pair['inputTokens'][0]['decimals'])))
+            token_price_in_usd = token_price_in_eth * eth_price
+            return token_price_in_usd
 
     def getWethPairForToken(self, token):
         query = self._load_query('get_eth_pair_for_token.graphql')
@@ -51,7 +90,7 @@ class PriceProvider:
         if response['markets'] == None:
             return None
 
-        return response['markets'][0]['id']
+        return response['markets'][0]
 
     def _load_query(self, path):
         with open(path) as f:
