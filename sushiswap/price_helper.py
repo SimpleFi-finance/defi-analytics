6863b9e3c768d85d42c5657e23ed4f540501383c
Goran Vladika <goran.vladika@gmail.com>
2022-06-15 14:52:29 +0200

    Add helper to find WETH pair for custom token

--- sushiswap/price_helper.py
+++ sushiswap/price_helper.py
@@ -8,7 +8,7 @@ WETH = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 class PriceProvider:
     def __init__(self):
         transport = AIOHTTPTransport(url=SUSHISWAP_ENDPOINT)
-        self._client = Client(transport=transport, fetch_schema_from_transport=True)
+        self._client = Client(transport=transport, fetch_schema_from_transport=True, execute_timeout=30)
 
     def getEthPriceinUSD(self, block):
         """Calculate ETH price in USD at a given block.
@@ -18,8 +18,11 @@ class PriceProvider:
         query = self._load_query('usdc_eth_reserves.graphql')
         vars = {"block": block, "market": USDC_ETH_PAIR}
         response = self._client.execute(query, variable_values=vars)
-        reserves = response['market']['inputTokenTotalBalances']
 
+        if response['market'] is None:
+            return 0
+
+        reserves = response['market']['inputTokenTotalBalances']
         tokenA = reserves[0].split("|")[0]
         tokenA_reserve = int(reserves[0].split("|")[2])
         tokenB_reserve = int(reserves[1].split("|")[2])
@@ -29,6 +32,26 @@ class PriceProvider:
         else:
             return (tokenA_reserve * pow(10, -6)) / (tokenB_reserve * pow(10, -18))
 
+
+    def getTokenPriceinUSD(self, token, block):
+        """Calculate custom token price in USD at a given block.
+        Sushiswap subgraph is used to fetch WETH-USDC pool reserves.
+        """
+
+        if token == WETH:
+            return self.getEthPriceinUSD(block)
+
+        #TODO
+
+    def getWethPairForToken(self, token):
+        query = self._load_query('get_eth_pair_for_token.graphql')
+        vars = {"token": token}
+        response = self._client.execute(query, variable_values=vars)
+
+        if response['markets'] == None:
+            return None
+
+        return response['markets'][0]['id']
 
     def _load_query(self, path):
         with open(path) as f:
