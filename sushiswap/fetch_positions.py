627fbc06f15f7f58a6816c2ee8d9e57b5a57228d
Goran Vladika <goran.vladika@gmail.com>
2022-06-16 22:51:34 +0200

    Add calcs for position net profitability

--- sushiswap/fetch_positions.py
+++ sushiswap/fetch_positions.py
@@ -1,11 +1,12 @@
 from gql import gql, Client
 from gql.transport.aiohttp import AIOHTTPTransport
+from price_helper import PriceProvider
 
 SUSHISWAP_ENDPOINT = "https://api.thegraph.com/subgraphs/name/simplefi-finance/sushiswap"
 transport = AIOHTTPTransport(url=SUSHISWAP_ENDPOINT)
 
 # Create a GraphQL client using the defined transport
-client = Client(transport=transport, fetch_schema_from_transport=True)
+client = Client(transport=transport, fetch_schema_from_transport=True, execute_timeout=120)
 
 # Provide a GraphQL query
 ### accountAddress: \"0x0291eb432cb4a2613a7415018933e3db45bcd769\", 
@@ -130,35 +131,96 @@ print("Number of positions after merging histories: ", len(merged_positions))
 print("Non: ", non)
 print("Skip: ", skip, "\n")
 
+
+
 print("------ EXTRACT TOKENS AND TOKEN AMOUNTS ---------")
+
+
+price_provider = PriceProvider()
 for pos_id in merged_positions.keys():
     txs = merged_positions[pos_id]
+
+    print('#######')
+    # print(txs)
+    print('----')
+
 
     first_tx = txs[0]
     last_tx = txs[-1]
 
+    position_start_block = int(first_tx[4])
+    position_end_block = int(last_tx[4])
+
     tokenA = first_tx[1][0].split("|")[0]
-    tokenA_amount_start = first_tx[1][0].split("|")[2]
-    tokenA_amount_end = last_tx[1][0].split("|")[2]
+    tokenA_amount_start = int(first_tx[1][0].split("|")[2]) * pow(10, (-1) * price_provider.decimals(tokenA))
+    tokenA_price_start = price_provider.getTokenPriceinUSD(tokenA, position_start_block)
+    if(tokenA_price_start == None):
+        continue
+
+    tokenA_amount_end = int(last_tx[1][0].split("|")[2]) * pow(10, (-1) * price_provider.decimals(tokenA))
+    tokenA_price_end = price_provider.getTokenPriceinUSD(tokenA, position_end_block)
+    if(tokenA_price_end == None):
+        continue
 
     tokenB = first_tx[1][1].split("|")[0]
-    tokenB_amount_start = first_tx[1][1].split("|")[2]
-    tokenB_amount_end = last_tx[1][1].split("|")[2]
+    tokenB_amount_start = int(first_tx[1][1].split("|")[2]) * pow(10, (-1) * price_provider.decimals(tokenB))
+    tokenB_price_start = price_provider.getTokenPriceinUSD(tokenB, position_start_block)
+    if(tokenB_price_start == None):
+        continue
 
-    position_start_block = first_tx[4]
-    position_end_block = last_tx[4]
+    tokenB_amount_end = int(last_tx[1][1].split("|")[2]) * pow(10, (-1) * price_provider.decimals(tokenB))
+    tokenB_price_end = price_provider.getTokenPriceinUSD(tokenB, position_end_block)
+    if(tokenB_price_end == None):
+        continue
 
-    print("Position: ", pos_id)
+    position_start_value = tokenA_amount_start * tokenA_price_start + tokenB_amount_start * tokenB_price_start
+    position_end_value = tokenA_amount_end * tokenA_price_end + tokenB_amount_end * tokenB_price_end
+    position_end_value_if_held = tokenA_amount_start * tokenA_price_end + tokenB_amount_start * tokenB_price_end
 
-    print("TokenA = ", tokenA)
-    print("TokenA start = ", tokenA_amount_start)
-    print("TokenA end = ", tokenA_amount_end)
+    print("User starts position with:")
+    print("tokenA: {0}".format(tokenA))
+    print("tokenA amount: {0:.2f}".format(tokenA_amount_start))
+    print("tokenA price: ${0:.2f}".format(tokenA_price_start))
 
-    print("TokenB = ", tokenB)
-    print("TokenB start = ", tokenB_amount_start)
-    print("TokenB end = ", tokenB_amount_end)
+    print("tokenB: {0}".format(tokenB))
+    print("tokenB amount: {0:.2f}".format(tokenB_amount_start))
+    print("tokenB price: {0:.2f}".format(tokenB_price_start))
+    print("Position value: ${0:.2f}\n".format(position_start_value))
 
-    print("Position start block = ", position_start_block)
-    print("Position end block = ", position_end_block)
 
-    print("\n")
+    print("User ends position with:")
+    print("tokenA amount: {0:.2f}".format(tokenA_amount_end))
+    print("tokenA price: ${0:.2f}".format(tokenA_price_end))
+
+    print("tokenB amount: {0:.2f}".format(tokenB_amount_end))
+    print("tokenB price: {0:.2f}".format(tokenB_price_end))
+    print("Position value: ${0:.2f}\n".format(position_end_value))
+
+    pool_net_gain = position_end_value - position_start_value
+    hodl_net_gain = position_end_value_if_held - position_start_value
+
+    print("HODL position value : ${0:.2f}".format(position_end_value_if_held))
+    print("Net gain investing in pool : ${0:.2f}".format(pool_net_gain))
+    print("Net gain if HODLed : ${0:.2f}".format(hodl_net_gain))
+
+
+    print('#######')
+
+
+
+    # print("Position: ", pos_id)
+
+    # print("TokenA = ", tokenA)
+    # print("TokenA start = ", tokenA_amount_start)
+    # print("TokenA end = ", tokenA_amount_end)
+
+    # print("TokenB = ", tokenB)
+    # print("TokenB start = ", tokenB_amount_start)
+    # print("TokenB end = ", tokenB_amount_end)
+
+    # print("Position start block = ", position_start_block)
+    # print("Position end block = ", position_end_block)
+
+    # print("\n")
+
+
