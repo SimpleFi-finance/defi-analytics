185067a2bab07e7a9b34ecc87b4c69cc56b8651d
Goran Vladika <goran.vladika@gmail.com>
2022-06-21 10:46:56 +0200

    Add counter function for profitable positions

--- sushiswap/sushi_analyzer.py
+++ sushiswap/sushi_analyzer.py
@@ -1,4 +1,4 @@
-from curses import raw
+import csv
 from position_handler import PositionHandler
 
 SUSHISWAP_ENDPOINT = "https://api.thegraph.com/subgraphs/name/simplefi-finance/sushiswap"
@@ -25,14 +25,40 @@ def analyze_pool(pool, filename):
     print("Stats written to {0}".format(filename))
 
 def analyze_dai_weth_pool():
-    analyze_pool(DAI_WETH_POOL, "stats/usdc-eth.csv")
+    analyze_pool(DAI_WETH_POOL, "stats/dai-eth.csv")
 
 def analyze_ldo_weth_pool():
     analyze_pool(LDO_WETH_POOL, "stats/ldo-weth.csv")
 
+def profitability_ratio(filename):
+    profitable = 0
+    non_profitable = 0
+    net_gain_profitable = 0
+    net_gain_non_profitable = 0
+    with open(filename, mode='r') as csv_file:
+        stats = csv.DictReader(csv_file)
+
+        for row in stats:
+            if float(row['pool_vs_hodl_roi']) > 0:
+                profitable += 1
+            else:
+                non_profitable += 1
+
+            if float(row['pool_roi']) > 0:
+                net_gain_profitable += 1
+            else:
+                net_gain_non_profitable += 1
+
+        print(profitable, non_profitable)
+        print(net_gain_profitable, net_gain_non_profitable)
+
+
+
 def main():
     #analyze_dai_weth_pool()
-    analyze_ldo_weth_pool()
+    #analyze_ldo_weth_pool()
+    profitability_ratio("stats/dai-eth.csv")
+    profitability_ratio("stats/ldo-weth.csv")
 
 
 
