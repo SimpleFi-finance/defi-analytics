6786a8e0a09644a669343b272f7515f8ec71475a
Goran Vladika <goran.vladika@gmail.com>
2022-07-08 16:08:13 +0200

    Properly label X-axis

--- sushiswap/protocol_analyzer.py
+++ sushiswap/protocol_analyzer.py
@@ -1,4 +1,25 @@
 #%%
+# Make name mappings
+pairs = {}
+pairs["0xc3d03e4f041fd4cd388c549ee2a29a9e5075882f"] = "DAI_WETH"
+pairs["0xc558f600b34a5f69dd2f0d06cb8a88d829b7420a"] = "LDO_WETH"
+pairs["0xd75ea151a61d06868e31f8988d28dfe5e9df57b4"] = "AAVE_WETH"
+pairs["0x397ff1542f962076d0bfe58ea045ffa2d347aca0"] = "USDC_WETH"
+pairs["0xceff51756c56ceffca006cd410b03ffc46dd3a58"] = "WBTC_WETH"
+pairs["0x06da0fd433c1a5d7a4faa01111c044910a184553"] = "USDT_WETH"
+pairs["0x088ee5007c98a9677165d78dd2109ae4a3d04d0c"] = "YFI_WETH_"
+pairs["0x6a091a3406e0073c3cd6340122143009adac0eda"] = "ILV_WETH_"
+pairs["0xd4e7a6e2d03e4e48dfc27dd3f46df1c176647e38"] = "TOKE_WETH"
+pairs["0x795065dcc9f64b5614c407a6efdc400da6221fb0"] = "SUSHI_WETH"
+pairs["0xe12af1218b4e9272e9628d7c7dc6354d137d024e"] = "BIT_WETH"
+pairs["0xc3f279090a47e80990fe3a9c30d24cb117ef91a8"] = "ALCX_WETH"
+pairs["0x0463a06fbc8bf28b3f120cd1bfc59483f099d332"] = "PUNK_WETH"
+pairs["0x4a86c01d67965f8cb3d0aaa2c655705e64097c31"] = "SYN_WETH"
+pairs["0x99b42f2b49c395d2a77d973f6009abb5d67da343"] = "YGG_WETH"
+pairs["0xdb06a76733528761eda47d356647297bc35a98bd"] = "JPEG_WETH"
+pairs["0x117d4288b3635021a3d612fe05a3cbf5c717fef2"] = "SRM_WETH"
+pairs["0x31503dcb60119a812fee820bb7042752019f2355"] = "COMP_WETH"
+
 # Load position data
 import pandas as pd
 
@@ -67,6 +88,9 @@ ax = pool_vs_hodl_mean.plot.bar(rot=90)
 
 ax.set_title('Average pool_vs_hodl_roi per pool', fontweight='bold', color= 'yellow');
 ax.tick_params(colors='yellow', which='both')
+
+pool_vs_hodl_labels = [pairs[pair_address.get_text()] for pair_address in ax.get_xticklabels()]
+ax.set_xticklabels(pool_vs_hodl_labels, rotation=60, ha='right')
 ax
 
 # %%
@@ -77,4 +101,10 @@ ax = net_gains_mean.plot.bar(rot=90)
 
 ax.set_title('Average net gain per pool', fontweight='bold', color= 'yellow');
 ax.tick_params(colors='yellow', which='both')
+
+net_gain_labels = [pairs[pair_address.get_text()] for pair_address in ax.get_xticklabels()]
+ax.set_xticklabels(net_gain_labels, rotation=60, ha='right')
 ax
+
+
+# %%
